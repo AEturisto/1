d2f6c943a405b7e62627172792ce17bbbf98a02e
AETuristo <alexey.eskin@bk.ru>
2023-02-05 17:24:11 +0500

    Добавлена проверка ввода пользователя.

--- 1/Program.cs
+++ 1/Program.cs
@@ -5,6 +5,18 @@
         static void Main(string[] args)
         {
             string userInput = Console.ReadLine();
+            List<char> restrictedChars = checkSymbols(userInput);
+
+            if (restrictedChars.Count > 0 )
+            {
+                Console.Write("Forbidden characters: ");
+                for (int i = 0; i < restrictedChars.Count; i++)
+                {
+                    Console.Write(restrictedChars[i] + " ");
+                }
+                return;
+            }
+
             int userInputCenter = userInput.Length / 2;
             if ( userInput.Length % 2 == 0) {
                 string substring1 = userInput.Substring( 0, userInputCenter );
@@ -25,6 +37,21 @@
                 newstr += str[str.Length - i];
             }
             return newstr;
+        }
+
+        static List<char> checkSymbols(string str)
+        {
+            char[] allowedChars = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            List<char> restrictedChars = new List<char>();
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ( !allowedChars.Contains(str[i]) )
+                {
+                    restrictedChars.Add(str[i]);
+                }
+            }
+            return restrictedChars;
+
         }
     }
 }
